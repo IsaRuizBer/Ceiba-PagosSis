a004d97740a70b0cad334c6939cfdf8748c3b47e
Isaura Ruiz <isaura.ruiz@ceiba.com.co>
2020-09-29 17:42:14 -0500

    Se realizan ajustes para que las pruebas integrales tomen  H2

--- backend/src/test/java/com/ceiba/adn/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/ceiba/adn/backend/BackendApplicationTests.java
@@ -2,8 +2,16 @@ package com.ceiba.adn.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootTest
+
+@EnableJpaRepositories(basePackages = "com.ceiba.adn")
+@PropertySource("test-application.properties")
+@EnableTransactionManagement
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BackendApplicationTests {
 
 	@Test
--- backend/src/test/java/com/ceiba/adn/backend/infraestructura/configuracion/conexion/ConfiguracionMemoria.java
+++ backend/src/test/java/com/ceiba/adn/backend/infraestructura/configuracion/conexion/ConfiguracionMemoria.java
@@ -1,4 +1,14 @@
 package com.ceiba.adn.backend.infraestructura.configuracion.conexion;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
 public class ConfiguracionMemoria {
+
+
 }
