90d9372b7be6e5be24fcab52efa2eb438a55c62e
Isaura Ruiz <isaura.ruiz@ceiba.com.co>
2020-10-06 13:06:26 -0500

    ajustes al configuracion ruta del driver

--- protractor/conf.js
+++ protractor/conf.js
@@ -2,5 +2,14 @@
 exports.config = {
   //framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js']
+  specs: ['spec.js'],
+  
+  directConnect: true,
+capabilities: {
+  'browserName': 'chrome',
+  'binary': 'C:/Program Files/Google/Chrome/Application/chrome.exe'
 }
+  
+
+}
+
